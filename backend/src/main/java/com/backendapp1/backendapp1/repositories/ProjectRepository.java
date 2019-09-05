f1aac5a77f2a245d7ad4f73457cedf8688c108a5
AdrianD94 <ady.drozman2007@gmail.com>
2019-09-05 22:29:23 +0300

    add repo and project class

---
+++ backend/src/main/java/com/backendapp1/backendapp1/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.backendapp1.backendapp1.repositories;
+
+import com.backendapp1.backendapp1.Domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
